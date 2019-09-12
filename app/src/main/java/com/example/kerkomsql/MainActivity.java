4730bf21de59866ca7198a36b8337ce15dc14d96
Putra <putresnn@gmail.com>
2019-09-12 08:07:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kerkomsql/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kerkomsql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inputdata);
+    }
+}
